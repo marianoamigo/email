b0cffd406242c74a03a7381d3b79ee1bb9b18d76
Mariano Amigo <marianoamigo93@gmail.com>
2025-06-11 20:27:35 -0300

    Agregando /home al controller

--- src/main/java/com/api/notifications/email/controller/EmailController.java
+++ src/main/java/com/api/notifications/email/controller/EmailController.java
@@ -6,10 +6,7 @@ import dto.NotificationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
@@ -17,6 +14,11 @@ public class EmailController {
 
     @Autowired
     EmailService emailService;
+
+    @GetMapping("/home")
+    public String home() {
+        return "Microservicio de Email operativo 🚀";
+    }
 
     @PostMapping("/send")
     public ResponseEntity<?> send(@RequestBody NotificationDTO notification) throws ErrorService {
